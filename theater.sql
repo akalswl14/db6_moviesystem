c1e2bcdf91b5bc08d2ea6cf0058228c952f9abc0
sjlee523 <55562875+sjlee523@users.noreply.github.com>
2019-11-19 07:41:43 +0900

    FK 에러 수정
    string이 오류나서 일단 integer로 대체

--- theater.sql
+++ theater.sql
@@ -1,40 +1,41 @@
-﻿CREATE TABLE `Staff` (
+CREATE TABLE `Staff` 
+(
 	`EmployeeID`	integer	NOT NULL,
-	`PW`	varchar2(20)	NULL,
-	`Name`	varchar2(50)	NULL,
+	`PW`	varchar(20)	NULL,
+	`Name`	varchar(50)	NULL,
 	`Birthday`	date	NULL,
-	`Sex`	varchar2(5)	NULL,
-	`Position`	varchar2(50)	NULL,
-	`Phonenumber`	varchar2(100)	NULL,
-	`Address`	varchar2(225)	NULL,
-	`Team`	varchar2(50)	NULL,
+	`Sex`	varchar(5)	NULL,
+	`Position`	varchar(50)	NULL,
+	`Phonenumber`	varchar(100)	NULL,
+	`Address`	varchar(225)	NULL,
+	`Team`	varchar(50)	NULL,
 	`JoinDate`	date	NULL,
 	`HolidayNumber`	integer	NULL,
-	`Department`	varchar2(50)	NULL,
-	`AccountNumber`	varchar2(50)	NULL,
-	`Bank`	varchar2(20)	NULL,
+	`Department`	varchar(50)	NULL,
+	`AccountNumber`	varchar(50)	NULL,
+	`Bank`	varchar(20)	NULL,
 	`Salary`	integer	NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `Holiday` (
 	`StartDate`	date	NOT NULL,
 	`EndDate`	date	NOT NULL,
 	`EmployeeID`	integer	NOT NULL,
-	`Type`	varchar2(20)	NULL,
-	`Year-Month`	varchar2(10)	NOT NULL
+	`Type`	varchar(20)	NULL,
+	`Year-Month`	varchar(10)	NOT NULL
 );
 
 CREATE TABLE `Commute` (
 	`Date`	day	NOT NULL,
 	`Time`	integer	NOT NULL,
 	`EmployeeID`	integer	NOT NULL,
-	`BarcodeNumber`	varchar2(20)	NULL,
-	`Year-Month`	varchar2(10)	NOT NULL
+	`BarcodeNumber`	varchar(20)	NULL,
+	`Year-Month`	varchar(10)	NOT NULL
 );
 
 CREATE TABLE `Monthly_work` (
-	`Year-Month`	varchar2(10)	NOT NULL,
+	`Year-Month`	varchar(10)	NOT NULL,
 	`EmployeeID`	integer	NOT NULL,
 	`GeneralWorkHours`	integer	NULL,
 	`Late`	integer	NULL,
@@ -44,7 +45,7 @@ CREATE TABLE `Monthly_work` (
 
 CREATE TABLE `Monthly_Salary` (
 	`EmployeeID`	integer	NOT NULL,
-	`Year-Month`	varchar2(10)	NOT NULL,
+	`Year-Month`	varchar(10)	NOT NULL,
 	`GeneralworkSalary`	integer	NULL,
 	`OverworkSalary`	integer	NULL,
 	`ExtraPay`	integer	NULL,
@@ -53,99 +54,99 @@ CREATE TABLE `Monthly_Salary` (
 
 CREATE TABLE `SaleRecords_Snackbar` (
 	`Date`	date	NOT NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
 	`Sale`	integer	NULL,
 	`Exchange`	integer	NULL,
 	`Refund`	integer	NULL
 );
 
 CREATE TABLE `Delivery` (
-	`OrderNumber`	varchar2(50)	NOT NULL,
-	`DeliveryNumber`	varchar2(50)	NULL,
+	`OrderNumber`	varchar(50)	NOT NULL,
+	`DeliveryNumber`	varchar(50)	NULL,
 	`ExceptDate`	date	NULL
 );
 
 CREATE TABLE `Department` (
 	`DepartmentNumber`	integer	NOT NULL,
-	`Name`	varchar2(50)	NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Name`	varchar(50)	NULL,
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `OrderReturnRequest` (
-	`OrderNumber`	varchar2(50)	NOT NULL,
+	`OrderNumber`	varchar(50)	NOT NULL,
 	`Date`	date	NULL,
 	`OrderAmount`	integer	NULL,
 	`ReturnAmount`	integer	NULL,
 	`TotalPrice`	integer	NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
 	`DepartmentNumber`	integer	NOT NULL,
 	`EachNumber`	integer	NOT NULL
 );
 
 CREATE TABLE `Product_Snackbar` (
-	`ProductNumber`	varchar2(50)	NOT NULL,
-	`Name`	varchar2(50)	NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
+	`Name`	varchar(50)	NULL,
 	`Stock`	integer	NULL,
 	`Price`	integer	NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `Product` (
-	`ProductNumber`	varchar2(50)	NOT NULL,
-	`Name`	varchar2(50)	NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
+	`Name`	varchar(50)	NULL,
 	`Price`	integer	NULL,
-	`Category`	varchar2(20)	NULL
+	`Category`	varchar(20)	NULL
 );
 
 CREATE TABLE `Location` (
 	`LocationNumber`	integer	NOT NULL,
-	`Name`	varchar2(30)	NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Name`	varchar(30)	NULL,
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `EachFacility` (
 	`EachNumber`	integer	NOT NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL,
-	`Status`	varchar2(50)	NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
+	`Status`	varchar(50)	NULL,
 	`BuyDate`	date	NULL,
 	`LocationNumber`	integer	NOT NULL
 );
 
 CREATE TABLE `Discard` (
 	`EachNumber`	integer	NOT NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
 	`RequestDate`	date	NULL,
-	`DiscardType`	varchar2(255)	NULL,
+	`DiscardType`	varchar(255)	NULL,
 	`LocationNumber`	integer	NULL,
-	`Status`	varchar2(20)	NULL,
+	`Status`	varchar(20)	NULL,
 	`DiscardDate`	date	NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `FacilityRepair` (
 	`DepartNumber`	integer	NOT NULL,
 	`EachNumber`	integer	NOT NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL,
+	`ProductNumber`	varchar(50)	NOT NULL,
 	`RequestDate`	date	NULL,
-	`Detail`	varchar2(255)	NULL
+	`Detail`	varchar(255)	NULL
 );
 
 CREATE TABLE `Movie` (
 	`MovieCode`	integer	NOT NULL,
-	`Name`	varchar2(50)	NULL,
+	`Name`	varchar(50)	NULL,
 	`Rate`	float	NULL,
-	`Genre`	String	NULL,
+	`Genre`	integer	NULL,
 	`BookingRate`	float	NULL,
 	`Type`	integer	NULL,
 	`ReleaseDate`	date(8)	NULL,
 	`RunningTime`	time(8)	NULL,
-	`Cast`	String	NULL,
+	`Cast`	integer	NULL,
 	`TotalAttendance`	integer	NULL,
 	`AgeLimit`	integer	NULL,
-	`Poster`	varchar2(50)	NULL,
+	`Poster`	varchar(50)	NULL,
 	`Story`	text	NULL,
 	`ScreenType`	integer	NULL,
-	`Director`	varchar2(50)	NULL
+	`Director`	varchar(50)	NULL
 );
 
 CREATE TABLE `ScreeningMovie` (
@@ -155,70 +156,70 @@ CREATE TABLE `ScreeningMovie` (
 	`Date`	date	NULL,
 	`TotalSeat`	integer	NULL,
 	`ReservedSeat`	integer	NULL,
-	`ScreeningSpace`	String	NULL,
+	`ScreeningSpace`	integer	NULL,
 	`MovieCode`	integer	NOT NULL,
-	`Branch`	varchar2(50)	NOT NULL
+	`Branch`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `Seat_ScreeningNumber` (
-	`SeatNumber`	String	NOT NULL,
+	`SeatNumber`	integer	NOT NULL,
 	`ReserveStatus`	boolean	NULL,
 	`ScreeningNumber`	integer	NOT NULL
 );
 
 CREATE TABLE `NonMemberInfo` (
-	`ReserveNumber`	varchar2(50)	NOT NULL,
-	`Name`	varchar2(50)	NULL,
-	`PW`	varchar2(128)	NULL,
+	`ReserveNumber`	varchar(50)	NOT NULL,
+	`Name`	varchar(50)	NULL,
+	`PW`	varchar(128)	NULL,
 	`Birthday`	date	NULL,
-	`PhoneNumber`	varchar2(100)	NULL
+	`PhoneNumber`	varchar(100)	NULL
 );
 
 CREATE TABLE `ReserveSeatInfo` (
-	`SeatNumber`	String	NOT NULL,
-	`PeopleType`	String	NULL,
-	`ReserveNumber`	varchar2(50)	NOT NULL
+	`SeatNumber`	integer	NOT NULL,
+	`PeopleType`	integer	NULL,
+	`ReserveNumber`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `ReservationInfo` (
-	`ReserveNumber`	varchar2(50)	NOT NULL,
+	`ReserveNumber`	varchar(50)	NOT NULL,
 	`MemberStatus`	boolean	NULL,
-	`PaymentNumber`	varchar2	NULL,
+	`PaymentNumber`	varchar	NULL,
 	`ReserveDate`	date	NULL,
-	`ReservePlace`	String	NULL,
+	`ReservePlace`	integer	NULL,
 	`TicketIssueStatus`	boolean	NULL,
 	`ReserveCancel`	integer	NULL,
 	`ScreeningNumber`	integer	NOT NULL
 );
 
 CREATE TABLE `PaymentInfo` (
-	`PaymentNumber`	varchar2(50)	NOT NULL,
-	`PaymentMethod`	varchar2(50)	NOT NULL,
+	`PaymentNumber`	varchar(50)	NOT NULL,
+	`PaymentMethod`	varchar(50)	NOT NULL,
 	`Amount`	integer	NULL,
-	`ReserveNumber`	varchar2(50)	NOT NULL
+	`ReserveNumber`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `MemberInfo` (
-	`ID`	varchar2(50)	NOT NULL,
-	`PW`	varchar2(128)	NULL,
-	`Name`	varchar2(50)	NULL,
-	`PhoneNumber`	varchar2(100)	NULL,
+	`ID`	varchar(50)	NOT NULL,
+	`PW`	varchar(128)	NULL,
+	`Name`	varchar(50)	NULL,
+	`PhoneNumber`	varchar(100)	NULL,
 	`Birthday`	date	NULL,
 	`Point`	integer	NULL,
 	`AccumulatedPoint`	integer	NULL
 );
 
 CREATE TABLE `Reservation_Member` (
-	`ReserveNumber`	varchar2(50)	NOT NULL,
-	`ID`	varchar2(50)	NOT NULL
+	`ReserveNumber`	varchar(50)	NOT NULL,
+	`ID`	varchar(50)	NOT NULL
 );
 
 CREATE TABLE `MovieBranch` (
-	`Branch`	varchar2(50)	NOT NULL,
-	'District'	varchar2(50) NOT NULL, /* 점포 속성 - 지역 */
-	`Name`	varchar2(50)	NULL,
+	`Branch`	varchar(50)	NOT NULL,
+	'District'	varchar(50) NOT NULL, /* 점포 속성 - 지역 */
+	`Name`	varchar(50)	NULL,
 	`EachNumber`	integer	NOT NULL,
-	`ProductNumber`	varchar2(50)	NOT NULL
+	`ProductNumber`	varchar(50)	NOT NULL
 );
 
 ALTER TABLE `Staff` ADD CONSTRAINT `PK_STAFF` PRIMARY KEY (
@@ -337,104 +338,104 @@ ALTER TABLE `Holiday` ADD CONSTRAINT `FK_Staff_TO_Holiday_1` FOREIGN KEY (
 	`EmployeeID`
 )
 REFERENCES `Staff` (
-	`EmployeeID`
+	`EmployeeID`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Commute` ADD CONSTRAINT `FK_Staff_TO_Commute_1` FOREIGN KEY (
 	`EmployeeID`
 )
 REFERENCES `Staff` (
-	`EmployeeID`
+	`EmployeeID`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Monthly_work` ADD CONSTRAINT `FK_Staff_TO_Monthly_work_1` FOREIGN KEY (
 	`EmployeeID`
 )
 REFERENCES `Staff` (
-	`EmployeeID`
+	`EmployeeID`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Monthly_Salary` ADD CONSTRAINT `FK_Staff_TO_Monthly_Salary_1` FOREIGN KEY (
 	`EmployeeID`
 )
 REFERENCES `Staff` (
-	`EmployeeID`
+	`EmployeeID`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Monthly_Salary` ADD CONSTRAINT `FK_Monthly_work_TO_Monthly_Salary_1` FOREIGN KEY (
 	`Year-Month`
 )
 REFERENCES `Monthly_work` (
-	`Year-Month`
+	`Year-Month`	ON DELETE CASCADE
 );
 
 ALTER TABLE `SaleRecords_Snackbar` ADD CONSTRAINT `FK_Product_Snackbar_TO_SaleRecords_Snackbar_1` FOREIGN KEY (
 	`ProductNumber`
 )
 REFERENCES `Product_Snackbar` (
-	`ProductNumber`
+	`ProductNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `OrderReturnRequest` ADD CONSTRAINT `FK_Delivery_TO_OrderReturnRequest_1` FOREIGN KEY (
 	`OrderNumber`
 )
 REFERENCES `Delivery` (
-	`OrderNumber`
+	`OrderNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `EachFacility` ADD CONSTRAINT `FK_Product_TO_EachFacility_1` FOREIGN KEY (
 	`ProductNumber`
 )
 REFERENCES `Product` (
-	`ProductNumber`
+	`ProductNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Discard` ADD CONSTRAINT `FK_EachFacility_TO_Discard_1` FOREIGN KEY (
 	`EachNumber`
 )
 REFERENCES `EachFacility` (
-	`EachNumber`
+	`EachNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Discard` ADD CONSTRAINT `FK_EachFacility_TO_Discard_2` FOREIGN KEY (
 	`ProductNumber`
 )
 REFERENCES `EachFacility` (
-	`ProductNumber`
+	`ProductNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `FacilityRepair` ADD CONSTRAINT `FK_EachFacility_TO_FacilityRepair_1` FOREIGN KEY (
 	`EachNumber`
 )
 REFERENCES `EachFacility` (
-	`EachNumber`
+	`EachNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `FacilityRepair` ADD CONSTRAINT `FK_EachFacility_TO_FacilityRepair_2` FOREIGN KEY (
 	`ProductNumber`
 )
 REFERENCES `EachFacility` (
-	`ProductNumber`
+	`ProductNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `NonMemberInfo` ADD CONSTRAINT `FK_ReservationInfo_TO_NonMemberInfo_1` FOREIGN KEY (
 	`ReserveNumber`
 )
 REFERENCES `ReservationInfo` (
-	`ReserveNumber`
+	`ReserveNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `ReserveSeatInfo` ADD CONSTRAINT `FK_Seat_ScreeningNumber_TO_ReserveSeatInfo_1` FOREIGN KEY (
 	`SeatNumber`
 )
 REFERENCES `Seat_ScreeningNumber` (
-	`SeatNumber`
+	`SeatNumber`	ON DELETE CASCADE
 );
 
 ALTER TABLE `Reservation_Member` ADD CONSTRAINT `FK_ReservationInfo_TO_Reservation_Member_1` FOREIGN KEY (
 	`ReserveNumber`
 )
 REFERENCES `ReservationInfo` (
-	`ReserveNumber`
+	`ReserveNumber`	ON DELETE CASCADE
 );
 
