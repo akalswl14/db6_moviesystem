a68966f672bd5c55273b03b95dea1019c03e2d2a
마민지 <gnkryd12@gmail.com>
2019-11-23 14:43:28 +0900

    Fix seat javascript
    좌석 자바스크립트 파일 완성

--- js/movieseat.js
+++ js/movieseat.js
@@ -1,6 +1,49 @@
+var arr = new Array();
+var adult = Number(document.getElementById("adult").value);
+var kid = Number(document.getElementById("kid").value);
+var banner_seat = document.getElementById("seatchoice");
+var banner_price = document.getElementById("totalprice");
+
+function numchoice(obj){
+    var tem_adult = Number(document.getElementById("adult").value);
+    var tem_kid = Number(document.getElementById("kid").value);
+    var len = document.getElementsByClassName("seat_val_num_selected").length;
+    if(tem_adult+tem_kid>8){
+        alert("인원을 초과하였습니다.최대인원은 8명 입니다.");
+        obj.value="00";
+        if(obj.id == "adult"){
+            tem_adult = 0;
+        }else{
+            tem_kid=0;
+        }
+    }
+    adult = tem_adult;
+    kid = tem_kid;
+    for(var i=0;i<len;i++){
+        document.getElementsByClassName("seat_val_num_selected")[0].className = "seat_val_num";
+    }
+    arr = new Array();
+    banner_seat.innerHTML = arr.toString();
+    banner_price.innerHTML = adult*8000+kid*7000;
+}
 function seatchoice(obj){
     var seat = obj.id;
-    var banner_seat = document.getElementById("seatchoice");
-    banner_seat.innerHTML = seat;
-    // 배열에 선택된거 넣어놓고, 선택된거 해제할때는 배열에서 뺴면서 배너로 업데이트 시켜주고, 배열 길이로 좌석수 판단
+    var idx = arr.indexOf(seat);
+    banner_seat = document.getElementById("seatchoice");
+    banner_price = document.getElementById("totalprice");
+    adult = Number(document.getElementById("adult").value);
+    kid = Number(document.getElementById("kid").value);
+    if(idx == -1){
+        if(adult+kid<=arr.length){
+            alert("인원을 초과하였습니다.");
+        }else{
+            arr.push(seat);
+            obj.className= "seat_val_num_selected";
+        }
+    }else{
+        arr.splice(idx,1);
+        obj.className= "seat_val_num";
+    }
+    banner_seat.innerHTML = arr.toString();
+    banner_price.innerHTML = adult*8000+kid*7000;
 }
